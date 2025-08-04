c64216b93a2e9f93e15352771b847f1711e5deed
adam-astiti <adamastitis.email@gmail.com>
2025-08-04 10:25:07 +0700

    update app

--- Streamlit_app/Introduction.py
+++ Streamlit_app/Introduction.py
@@ -18,17 +18,6 @@ st.title("A Data Analyst Job Market 2023")
 
 with st.sidebar:
     st.image(r'Streamlit_app/streamlit-logo-primary-colormark-darktext.png', use_container_width=False)
-    page = st.radio(
-        "Go to:",
-        (
-            "Introduction",
-            "1: The Job Market Landscape",
-            "2: The Analyst's Toolkit",
-            "3: Salary Insights",
-            "4: Remote Work Landscape",
-            "5: Conclusion"
-        )
-    )
     st.write("""### Project by: **Adam Astiti**""")
     st.link_button('Linkedin', 'https://www.linkedin.com/in/adam-astiti-a3787312a/', icon='💼')
     st.link_button('Github', 'https://github.com/adam-astiti', icon='👨‍💻')
@@ -47,5 +36,6 @@ st.markdown("""
     Use the navigation in the sidebar to explore the different chapters of this analysis.
     This project was built using Python, SQL, and Streamlit, with the data processed in a custom-built data warehouse.
     """)
+
 
 
