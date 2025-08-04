e9b3672c86e6057b8e3174502f73456e933a58f4
adam-astiti <adamastitis.email@gmail.com>
2025-08-04 10:01:32 +0700

    Update Introduction.py

--- Streamlit_app/Introduction.py
+++ Streamlit_app/Introduction.py
@@ -15,7 +15,7 @@ st.title("A Data Analyst Job Market 2023")
 # Sidebar
 
 with st.sidebar:
-    st.image('Streamlit_app\streamlit-logo-primary-colormark-darktext.png', use_container_width=False)
+    st.image(r'Streamlit_app/streamlit-logo-primary-colormark-darktext.png', use_container_width=False)
     st.write("""### Project by: **Adam Astiti**""")
     st.link_button('Linkedin', 'https://www.linkedin.com/in/adam-astiti-a3787312a/', icon='💼')
     st.link_button('Github', 'https://github.com/adam-astiti', icon='👨‍💻')
